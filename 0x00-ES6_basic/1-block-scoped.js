cf9836126a28ffc8dad78b2bc547ffd82dc11677
lawden <lawtaden@gmail.com>
2024-06-26 01:31:07 +0200

    scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
-// 1-block-scoped.js
 export default function taskBlock(trueOrFalse) {
   let task = false;
   let task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    task = false;
+    task2 = true;
   }
 
   return [task, task2];
