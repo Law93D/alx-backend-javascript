4d4df4830c5f047e698b396559717b2d5c1dc082
lawden <lawtaden@gmail.com>
2024-06-26 07:30:37 +0200

    report

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+// 12-createReportObject.js
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
