4bd379a79e3dbbfb9292b3e7bd4ddc430595f88f
lawden <lawtaden@gmail.com>
2024-06-28 00:48:26 +0200

    building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,14 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') throw new TypeError('Square feet must be a number');
+    this._sqft = sqft;
+
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
