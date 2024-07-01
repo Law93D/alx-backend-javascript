93ec0361b9f5c02d275f2e3cefdf549135bbec3c
lawden <lawtaden@gmail.com>
2024-07-01 21:00:51 +0200

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+// 8-clean_set.js
+export default function cleanSet(set, startString) {
+  if (startString === '') return '';
+
+  const result = [];
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result.push(value.slice(startString.length));
+    }
+  });
+
+  return result.join('-');
+}
