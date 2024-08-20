97d23c1ce781f7fc5e9ded56703dd5adb41620f8
lawden <lawtaden@gmail.com>
2024-08-20 10:03:39 +0200

    process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,11 @@
-process.stdin.setEncoding('utf8');
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-process.stdin.on('readable', () => {
-    const chunk = process.stdin.read();
-    if (chunk !== null) {
-        process.stdout.write(chunk);
-    }
+process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    console.log(`Your name is: ${name}`);
+    process.exit();
 });
 
-process.stdin.on('end', () => {
-    process.stdout.write('End of input\n');
+process.on('exit', () => {
+    console.log("This important software is now closing");
 });
