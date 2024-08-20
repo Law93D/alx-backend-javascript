4ad940c5b5fb109e81d017b7daef15033e329f89
lawden <lawtaden@gmail.com>
2024-08-20 10:20:09 +0200

    process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,10 +2,10 @@ process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on('data', (data) => {
     const name = data.toString().trim();
-    process.stdout.write(`Your name is: ${name}\n`);
-    process.exit();
+    console.log(`Your name is: ${name}`);
+    process.exit(0);
 });
 
 process.on('exit', () => {
-    console.log("This important software is now closing");
+    console.log("This important software is now closing\n");
 });
